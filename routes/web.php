85ca61a4bd8478f235f5977f998f6536e1a27312
vaibhav <vaibhav@dgnote.com>
2020-01-05 07:34:15 +0530

    make changes for default url

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource('contact', 'ContactController');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::resource('/', 'ContactController');
 
 
   
